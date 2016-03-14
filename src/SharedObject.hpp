8f0e0ce4603699dbd6b73af47485d21836195f71
Remco <remco-95@live.nl>
2016-03-14 15:12:36 +0100

    Moved Accessor outside of SharedObject class

--- src/SharedObject.hpp
+++ src/SharedObject.hpp
@@ -8,25 +8,28 @@ class SharedObject : public NotCopyable {
 public:
 	SharedObject(T & data_input) : data(data_input) {}
 
-	class Accessor : public NotCopyable {
-	public:
-
-		Accessor(SharedObject<T> & so_input) : so(so_input) { so.claim(); }
-
-		~Accessor() { so.release(); }
-
-		T& access() { return so; }
-
-	private:
-		SharedObject& so;
-	};
+	
 
 protected:
 	T& data; // SharedData only accesible by Accessor
-	friend class Accessor;
+	template<typename T> friend class Accessor;
 
-	virtual void claim() {}
-	virtual void release() {}
+	virtual void claim() = 0;
+	virtual void release() = 0;
+};
+
+template<typename T>
+class Accessor : public NotCopyable {
+public:
+
+	Accessor(SharedObject<T> & so_input) : so(so_input) { so.claim(); }
+
+	~Accessor() { so.release(); }
+
+	T& access() { return so.data; }
+
+private:
+	SharedObject& so;
 };
 
 #endif
