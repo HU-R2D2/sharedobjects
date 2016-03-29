736e993b67e65cff497981182ae3fb4252c14309
Koen van der Kruk <koenvdkruk@gmail.com>
2016-03-29 15:25:48 +0200

    Documented NotCopyable

--- source/include/LoggingSharedObject.hpp
+++ source/include/LoggingSharedObject.hpp
@@ -5,6 +5,12 @@
 #include <mutex>
 #include <iostream>
 
+/// Class LoggingSharedObject
+/// This class is used to log when an object is claimed and released. 
+/// \authors Alexander Freeman, Koen van der Kruk, Timo Assen, Christiaan van den Berg, Remco Ruttenberg
+/// \version 1.0
+/// \date 29-3-2016
+
 template<typename T>
 class LoggingSharedObject : public SharedObject<T> {
 public:
--- source/include/NotCopyable.hpp
+++ source/include/NotCopyable.hpp
@@ -1,14 +1,28 @@
 #ifndef NO_COPY_H
 #define NO_COPY_H
 
-
+/// Class NotCopyable
+/// Thic class is used to make sure the inherentenced classes can not be copied
+/// \authors Alexander Freeman, Koen van der Kruk, Timo Assen, Christiaan van den Berg, Remco Ruttenberg
+/// \version 1.0
+/// \date 29-3-2016
 class NotCopyable {
 protected:
-   NotCopyable() = default;
-    ~NotCopyable() = default;
 
-   NotCopyable( const NotCopyable& ) = delete;
-   NotCopyable& operator=( const NotCopyable& ) = delete;
+	/// Prevents the class from using the default constructor
+	NotCopyable() = default;
+
+	/// Prevents the class from being destructed
+	~NotCopyable() = default;
+
+	/// Prevents the class from being copied into a new object
+	/// \param The origin class
+	NotCopyable(const NotCopyable&) = delete;
+
+	/// Prevents the assignment of the inheritanced class
+	/// \param The origin class
+	/// \return A new copy of the class
+	NotCopyable& operator=(const NotCopyable&) = delete;
 };
 
 #endif
