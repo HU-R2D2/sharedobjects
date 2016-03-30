2131757ff7449bcb806c285609fe90a80621a530
Remco-Ruttenberg <remco.ruttenberg@outlook.com>
2016-03-30 12:53:40 +0200

    Replaced tabs with spaces and fixed several comments

--- source/include/NotCopyable.hpp
+++ source/include/NotCopyable.hpp
@@ -1,28 +1,25 @@
 #ifndef NO_COPY_H
 #define NO_COPY_H
 
-/// Class NotCopyable
-/// Thic class is used to make sure the inherentenced classes can not be copied
-/// \authors Alexander Freeman, Koen van der Kruk, Timo Assen, Christiaan van den Berg, Remco Ruttenberg
+/// Prevents copy and assignment operations
+/// Thic class is used to make sure the inherentenced classes can not be copied or assigned.
+/// \author Alexander Freeman, Koen van der Kruk, Timo Assen, Christiaan van den Berg, Remco Ruttenberg
 /// \version 1.0
 /// \date 29-3-2016
 class NotCopyable {
 protected:
 
-	/// Prevents the class from using the default constructor
-	NotCopyable() = default;
+   /// Forces the use of the default constructor (compiler generated)
+   NotCopyable() = default;
 
-	/// Prevents the class from being destructed
-	~NotCopyable() = default;
+   /// Forces the use of the default destructor (compiler generated)
+   ~NotCopyable() = default;
 
-	/// Prevents the class from being copied into a new object
-	/// \param The origin class
-	NotCopyable(const NotCopyable&) = delete;
+   /// Prevents the class from being copied into a new object
+   NotCopyable(const NotCopyable&) = delete;
 
-	/// Prevents the assignment of the inheritanced class
-	/// \param The origin class
-	/// \return A new copy of the class
-	NotCopyable& operator=(const NotCopyable&) = delete;
+   /// Prevents the class from being assigned
+   NotCopyable& operator=(const NotCopyable&) = delete;
 };
 
 #endif
